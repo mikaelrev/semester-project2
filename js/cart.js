c168d860d29c5192da13d8e027c756aea57fe80a
mikaelrev <mikael.myrvang@gmail.com>
2022-05-19 15:42:12 +0200

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -14,7 +14,7 @@ if(cartProducts.length === 0) {
 
 cartProducts.forEach((product) => {
     cartContainer.innerHTML += `<div class="card" style="width: 18rem;">
-                                    <img src="${product.image}" class="card-img-top" alt="${product.image.alternativeText}">
+                                    <img src="${product.image.url}" class="card-img-top" alt="${product.image.alternativeText}">
                                     <div class="card-body">
                                         <h5 class="card-title">${product.title}</h5>
                                         <p class="card-text">${product.price}$</p>
